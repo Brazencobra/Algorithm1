997861d499b7bef8b12fc07322e820636bfc5dec
Brazencobra <elxan.memmedli2003@gmail.com>
2022-10-30 23:01:08 +0400

    Test

---
+++ task3.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace Task3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            char Herf;
+            Console.WriteLine("Azerbaycan dili ucun-a | Rus dili ucun-r herfini daxil edin:");
+            Herf = Convert.ToChar(Console.ReadLine());
+            if (true)
+            {
+
+                   
+                if (Herf == 'a')
+                {
+                    Console.WriteLine("Azerbaycan dilini secdiniz");
+                } 
+                if (Herf == 'A')
+                {
+                    Console.WriteLine("Azerbaycan dilini secdiniz");
+                }
+                if (Herf == 'R')
+                {
+                    Console.WriteLine("Rus dilini secdiniz");
+                }
+                if (Herf == 'r')
+                {
+                    Console.WriteLine("Rus dilini secdiniz");
+                }
+                
+            }
+            else
+            {
+                Console.WriteLine("Yanlis sorgu");
+            }
+        }
+    }
+}
